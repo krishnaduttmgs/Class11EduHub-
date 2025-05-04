a47c8360317160011bda38c73982ae014c8abd9d
krishnaduttmgs <krishnaduttmgs@gmail.com>
2025-05-04 18:54:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Page load hone par welcome message show karein
+window.onload = function() {
+    alert("Welcome to the Class 11 Educational Website!");
+};
+
+// Button click par subject ka message show karein
+function showSubject(subject) {
+    alert("You selected: " + subject);
+}
